bf3fd1aeddb6bfafa5879654f36d5a79035c6328
Omama Mahmood <98207789+runamoto@users.noreply.github.com>
2025-03-08 01:34:06 -0500

    A CAPITAL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const audio = new audio(
+const audio = new Audio(
   "https://attachments.are.na/35069181/6025338f4d57c46ac51f9bde5d9e3aaa.mp3?1741414422"
 );
 const buttons = document.querySelectorAll("button");
