7d1fc238db7df478a1fd37cb3d6a49747a0c868f
Omama Mahmood <98207789+runamoto@users.noreply.github.com>
2025-03-08 01:41:39 -0500

    PLEASE WORK

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const audio = new Audio(
+console.log("what the ★");
+
+let audio = new Audio(
   "https://attachments.are.na/35069181/6025338f4d57c46ac51f9bde5d9e3aaa.mp3?1741414422"
 );
-const buttons = document.querySelectorAll("*");
-buttons.forEach((button) => {
+document.querySelectorAll("*").forEach((button) => {
   button.addEventListener("click", () => {
     audio.play();
   });
