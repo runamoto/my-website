a08a298a058dad1a7e2efea8388747415944c9fe
Omama Mahmood <98207789+runamoto@users.noreply.github.com>
2025-03-08 01:32:40 -0500

    added twig sound on-click

---
+++ script.js
@@ -0,0 +1,9 @@
+const audio = new audio(
+  "https://attachments.are.na/35069181/6025338f4d57c46ac51f9bde5d9e3aaa.mp3?1741414422"
+);
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    audio.play();
+  });
+});
