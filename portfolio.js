1ad6be568a17ac201553b1b44377f2498ffa3cd2
Aaryan Pashine <pashineaaryan@gmail.com>
2025-04-27 12:20:25 -0400

    good pictures

--- portfolio.js
+++ portfolio.js
@@ -2,6 +2,9 @@ function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("p5");
 }
+
+let blocks = []
+
 function render_channel(channel) {
   let blocks = channel.contents.map(render_block).join("");
 
@@ -23,6 +26,7 @@ function render_channel(channel) {
 }
 
 function render_block(block) {
+  blocks.push(block)
   if (block.class == "Image") return image(block);
   if (block.class == "Attachment" && block.attachment.extension == "mp4")
     return mp4(block);
@@ -30,7 +34,7 @@ function render_block(block) {
 
 function image(block) {
   return `
-    <div class="block fullsizeable">
+    <div class="block fullsizeable" block-id="${block.id}">
       <img src= ${block.image?.display.url}></img>
     </div>`;
 }
@@ -47,7 +51,17 @@ let init = (channels) => channels.forEach(render_channel);
 let mountonclick = () => {
   document.querySelectorAll(".fullsizeable").forEach((e) => {
     e.onclick = (btn) => {
-      e.classList.toggle("fullsize")
+      // make block if fullsizeable
+      let fullsize = document.createElement("div")
+      fullsize.classList.add("fullsize")
+      let image = document.createElement("img")
+      image.src = blocks.find(b => b.id == e.getAttribute("block-id")).image?.original.url
+
+      fullsize.appendChild(image)
+      fullsize.onclick = () => fullsize.remove()
+      document.body.appendChild(fullsize)
+
+      //e.classList.toggle("fullsize")
     }
   })
 }
