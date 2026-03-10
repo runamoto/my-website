51e55b270ad9c6743c6ed8127d8cc469d55ad6fb
Omama <omamamahmood6@gmail.com>
2026-03-10 16:03:59 -0400

    saving

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -4,14 +4,14 @@ function setup() {
 }
 
 let blocks = [];
+let channels = {}
 
 function render_channel(channel) {
-  console.log(channel)
-  let blocks = channel.contents.map(render_block).join("");
+  channels[channel.slug] = channel
+  let blocks = channel.contents.slice(0, 1).map(render_block).join("");
 
-  console.log(channel);
   document.querySelector(".project-container").innerHTML += `
-    <div class="channel" tags="#all ${channel?.metadata?.description}">
+    <div class="channel" id="channel-${channel?.slug}" tags="#all ${channel?.metadata?.description}">
       <div class="metadata-container" >
         <h1> ${channel.title}</h1>
         <p>${
@@ -25,6 +25,43 @@ function render_channel(channel) {
       </div>
     </div>
     `;
+
+}
+
+function renderFullPage(channel){
+  let blocks = channel.contents.map(render_block).join("");
+  let button = document.createElement('button')
+  button.classList.add('close-button')
+  button.innerText = "close"
+  button.onclick = closeFullPage
+    // code to make the full page using channel contents
+  let fullPage = document.createElement('div')
+
+  let contents = document.createElement('div')
+  contents.innerHTML = `
+    <div>
+    <h1>${channel.title}</h1>
+      <p>${
+          channel?.contents.find(function (block) {
+            if (block.title == "description") return true;
+          })?.content_html
+        }</p>
+      <div class="image-container">
+        ${blocks}
+      </div>
+    </div>
+    `;
+
+  fullPage.appendChild(button)
+  fullPage.appendChild(contents)
+  fullPage.classList.add('full-page')
+
+    document.body.appendChild(fullPage)
+}
+
+function closeFullPage(){
+  console.log('called')
+  document.querySelector('.full-page').remove()
 }
 
 function render_block(block) {
@@ -67,6 +104,13 @@ let mountonclick = () => {
       //e.classList.toggle("fullsize")
     };
   });
+
+  Object.entries(channels).forEach(([slug, channel]) => {
+    let el = document.querySelector("#channel-"+slug)
+    el.onclick = () => {
+      renderFullPage(channel)
+    }
+  })
 };
 
 fetch("./data.json")
@@ -76,6 +120,10 @@ fetch("./data.json")
 
 function tagButton(tag){ console.log(tag);
   let button = document.getElementById(tag);
+  if(!button) {
+    console.log("DIDN/t find", tag)
+    return
+  }
   button.onclick = () => {
     document.querySelectorAll('.channel').forEach((channel) => {
       let tags = channel.getAttribute('tags');
@@ -95,8 +143,8 @@ tagButton('all');
 tagButton('typography');
 tagButton('ecology');
 tagButton('archiving');
-tagButton('photography');
-tagButton('writing');
+// tagButton('photography');
+// tagButton('writing');
 
 tagButton('printmaking&textiles');
 tagButton('books');
